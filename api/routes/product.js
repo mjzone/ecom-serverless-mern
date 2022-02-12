0e097c7ff855371e2046b3db2abca3b0f6123a43
Manoj Fernando <manojf@99x.lk>
2022-02-12 15:03:12 +0530

    Added product and user controllers

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,4 +1,111 @@
+const Product = require("../models/Product");
+const CryptoJS = require("crypto-js");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// POST Product
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// PUT Product
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE Product
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET USER
+router.get("/find/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET ALL Products
+router.get("/", async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    try {
+        let products;
+        if (qNew) {
+            products = Product.find().sort({ craetedAt: -1 }).limit(5);
+        } else if (qCategory) {
+            products = Product.find({
+                categories: {
+                    $in: [qCategory]
+                }
+            })
+        } else {
+            products = new Product.find();
+        }
+        res.status(200).json(products);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+// // GET USER STATS
+// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+//     try {
+//         const date = new Date();
+//         const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+//         const data = await User.aggregate([
+//             {
+//                 // First get all users matching the below query
+//                 $match: {
+//                     createdAt: { $gte: lastYear }
+//                 }
+//             },
+//             {
+//                 // Project only following attributes. Attributes could be already exising values or computed value like below. E.g. month variable
+//                 $project: {
+//                     month: { $month: "$createdAt" }
+//                 }
+//             },
+//             {
+//                 // Group the items and return aggregated results
+//                 $group: {
+//                     _id: "$month", // group by month variable we created above
+//                     total: { $sum: 1 } // calculate every registered user in each month
+//                 }
+//             }
+//         ]);
+//         res.status(200).json(data);
+
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -34,9 +34,9 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
     const query = req.query.new;
     try {
-        const users = query 
-                ? await User.find().sort({_id: -1}).limit(1)
-                : await User.find();
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(1)
+            : await User.find();
 
         const usersWithoutPasswords = users.map(u => {
             return {
@@ -56,6 +56,40 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const date = new Date();
+        const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+        const data = await User.aggregate([
+            {
+                // First get all users matching the below query
+                $match: {
+                    createdAt: { $gte: lastYear }
+                }
+            },
+            {
+                // Project only following attributes. Attributes could be already exising values or computed value like below. E.g. month variable
+                $project: {
+                    month: { $month: "$createdAt" }
+                }
+            },
+            {
+                // Group the items and return aggregated results
+                $group: {
+                    _id: "$month", // group by month variable we created above
+                    total: { $sum: 1 } // calculate every registered user in each month
+                }
+            }
+        ]);
+        res.status(200).json(data);
+
     } catch (err) {
         res.status(500).json(err);
     }
